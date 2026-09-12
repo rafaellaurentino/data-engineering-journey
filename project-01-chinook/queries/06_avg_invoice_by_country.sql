b079fe2462173ae2e3ee7f38916f7d6eb5a4f21b
Rafael Laurentino <rafaelgdroidinfor@gmail.com>
2026-09-11 22:01:06 -0300

    changes: removed older query related to the total revenue by country

---
+++ project-01-chinook/queries/06_avg_invoice_by_country.sql
@@ -0,0 +1,19 @@
+/*
+
+For each country, what is the average invoice total? 
+Which country has the highest average order value?
+
+*/
+
+SELECT
+    *
+FROM invoice;
+--after we o took a look at the invoice table, we can see that we´ll be needing only
+
+--Which country has the highest average order value?
+SELECT
+    i.billing_country,
+    AVG(i.total) AS avg
+FROM invoice i
+GROUP BY i.billing_country
+ORDER BY avg DESC;
