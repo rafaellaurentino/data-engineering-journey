a57784dcdfae1849d7b8386c285a2c2c1c4ae243
Rafael Laurentino <rafaelgdroidinfor@gmail.com>
2026-09-19 23:47:15 -0300

    changes: half query fixed

--- project-02-olist/setup/olist_setup.sql
+++ project-02-olist/setup/olist_setup.sql
@@ -14,10 +14,10 @@ BEFORE WE BEGIN, KEEP IN MIND THAT THE ORDER WE CREATE THE TABLES HAS SOME RULES
 --customers table
 
 CREATE TABLE IF NOT EXISTS customers (
-	customer_id VARCHAR(),
-	customer_unique_id VARCHAR(),
-	customer_zip_code_prefix VARCHAR(),
-	customer_city TEXT(),
+	customer_id VARCHAR(32),
+	customer_unique_id VARCHAR(32),
+	customer_zip_code_prefix VARCHAR(5),
+	customer_city TEXT,
 	customer_state CHAR(2)
 );
 
@@ -47,10 +47,10 @@ CREATE TABLE IF NOT EXISTS products (
 --geolocation table
 
 CREATE TABLE IF NOT EXISTS geolocation (
-	geolocation_zip_code_prefix CHAR(4),
-	geolocation_lat NUMERIC(9,15),
-	geolocation_lng NUMERIC(9,15),
-	geolocation_city VARCHAR(),
+	geolocation_zip_code_prefix CHAR(5),
+	geolocation_lat NUMERIC(17,15),
+	geolocation_lng NUMERIC(17,15),
+	geolocation_city TEXT,
 	geolocation_state CHAR(2)
 );
 
@@ -64,48 +64,62 @@ CREATE TABLE IF NOT EXISTS product_category_name (
 --orders table
 
 CREATE TABLE IF NOT EXISTS orders (
-	order_id VARCHAR(),
-	customer_id VARCHAR(),
-	order_status VARCHAR(),
+	order_id VARCHAR(32),
+	customer_id VARCHAR(32) NOT NULL,
+	order_status VARCHAR(20),
 	order_purchase_timestamp TIMESTAMP,
     order_approved_at TIMESTAMP,
     order_delivered_carrier_date TIMESTAMP,
     order_delivered_customer_date TIMESTAMP,
     order_estimated_delivery_date TIMESTAMP,
+
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
 );
 
 --order_items table
 
 CREATE TABLE IF NOT EXISTS order_items (
-	order_id VARCHAR(),
+	order_id VARCHAR(32),
 	order_item_id INT,
-	product_id VARCHAR(),
-	seller_id VARCHAR(),
+	product_id VARCHAR(32) NOT NULL,
+	seller_id VARCHAR(32) NOT NULL,
 	shipping_limit_date TIMESTAMP,
-	price DECIMAL(10,2),
-	freight_value DECIMAL(10,2)
+	price DECIMAL(10,2) NOT NULL,
+	freight_value DECIMAL(10,2) NOT NULL,
+
+    PRIMARY KEY (order_id, order_item_id),
+
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (product_id) REFERENCES products(product_id),
+    FOREIGN KEY (seller_id) REFERENCES sellers(seller_id)
 );
 
 --order_payments table
 
 CREATE TABLE IF NOT EXISTS order_payments (
-	order_id VARCHAR(),
+	order_id VARCHAR(32),
 	payment_sequential INT,
-	payment_type VARCHAR(),
+	payment_type VARCHAR(20),
 	payment_installments INT,
-	payment_value DECIMAL(10,2)
+	payment_value DECIMAL(10,2),
+
+    PRIMARY KEY (order_id, payment_sequential),
+
+    FOREIGN KEY (order_id) REFERENCES orders(order_id)
 );
 
 --order_reviews table
 
 CREATE TABLE IF NOT EXISTS order_reviews (
-	review_id VARCHAR(),
-	order_id VARCHAR(),
-	review_score INT(1),
-	review_comment_title VARCHAR(),
-	review_comment_message VARCHAR(),
+	review_id VARCHAR(32),
+	order_id VARCHAR(32),
+	review_score INT CHECK (review_score BETWEEN 1 AND 5),
+	review_comment_title TEXT,
+	review_comment_message TEXT,
 	review_creation_date TIMESTAMP,
-	review_answer_timestamp TIMESTAMP
+	review_answer_timestamp TIMESTAMP,
+
+    FOREIGN KEY (order_id) REFERENCES orders(order_id)
 );
 
 
